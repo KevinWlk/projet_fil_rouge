9788c3dd6316ce50ae12f07c4d779ae24d057c19
KevinWlk <kevin.walencik@gmail.com>
2023-03-07 17:46:11 +0100

    ok

---
+++ Site/Planning/script.js
@@ -0,0 +1,45 @@
+
+let evenements = [{
+    "title": "Oral Blanc - DevWeb", 
+    "start": "2023-03-09 09:00:00",
+    "end": "2023-03-09 12:00:00",
+    "backgroundColor": "red",
+},{
+    "title": "PHP", 
+    "start": "2023-03-09 13:30:00",
+    "end": "2023-03-09 17:00:00",
+}]
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const calendarEl = document.getElementById("calendar");
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "timeGridWeek", 
+        locale : "fr",
+        headerToolbar : {
+            left:"prev,next today", 
+            center:"title",
+            right:"dayGridMonth, timeGridWeek, list",
+        },
+        buttonText:{
+            today: `Aujourd'hui`, 
+            month: `Mois`, 
+            week: `Semaine`, 
+            list: `Liste`, 
+        },
+        events: evenements,
+        nowIndicator: true,
+        editable: true,
+        eventDrop: (infos)=> {
+            if(!confirm("Êtes-vous sûr de vouloir déplacer l'évènement?")){
+                infos.revert();
+            }
+        },
+        eventResize: (infos)=> {
+            console.log(infos.event.end);
+        }
+    });
+
+    calendar.render();
+
+});
