172ced66b5a647d208fb63d89f62f4d650846aeb
KevinWlk <kevin.walencik@gmail.com>
2023-03-08 23:15:45 +0100

    08mars

--- Planning/script.js
+++ Planning/script.js
@@ -4,7 +4,7 @@ let evenements = [{
     "start": "2023-03-09 09:00:00",
     "end": "2023-03-09 12:00:00",
     "backgroundColor": "red",
-},{
+},{   
     "title": "PHP", 
     "start": "2023-03-09 13:30:00",
     "end": "2023-03-09 17:00:00",
@@ -14,12 +14,13 @@ let evenements = [{
 document.addEventListener("DOMContentLoaded", function() {
     const calendarEl = document.getElementById("calendar");
     const calendar = new FullCalendar.Calendar(calendarEl, {
+        height: "100%",
         initialView: "timeGridWeek", 
         locale : "fr",
         headerToolbar : {
-            left:"prev,next today", 
+            left:"prev next today", 
             center:"title",
-            right:"dayGridMonth, timeGridWeek, list",
+            right:"dayGridMonth timeGridWeek",
         },
         buttonText:{
             today: `Aujourd'hui`, 
@@ -30,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function() {
         events: evenements,
         nowIndicator: true,
         editable: true,
+        selectable: true,
+        selectHelper: true,
+        selectable: true,
         eventDrop: (infos)=> {
             if(!confirm("Êtes-vous sûr de vouloir déplacer l'évènement?")){
                 infos.revert();
@@ -42,4 +46,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     calendar.render();
 
+});
+
+var burger = document.querySelector('.burger');
+var sidebarNav = document.querySelector('.sidebarnav');
+
+burger.addEventListener('click', function() {
+sidebarNav.classList.toggle('show');
 });
